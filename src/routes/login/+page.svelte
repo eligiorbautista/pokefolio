60b7347f462ceb5e688660e6fa8bc3ee241aa112
Eli Bautista <dev.elibautista@gmail.com>
2025-07-02 01:56:17 +0800

    -a

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -37,7 +37,7 @@
                 toast.error(errorMessage);
             } else {
                 toast.success("Redirecting to your PokeFolio...");
-                await goto(`/${session?.user.email}`);
+                await goto(`/${session?.user?.email}`);
             }
         } catch (err) {
             errorMessage = "An unexpected error occurred";
